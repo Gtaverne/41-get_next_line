f0bee717ab940c75ac05f4bc539c4be7061ec2c1
Georges Taverne <gtaverne@student.42.fr>
2020-11-26 18:54:17 +0100

    with bonus

---
+++ get_next_line_bonus.h
@@ -0,0 +1,34 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/23 16:51:14 by user42            #+#    #+#             */
+/*   Updated: 2020/11/25 10:20:49 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <fcntl.h>
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <sys/stat.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+int		get_next_line(int fd, char **line);
+size_t	ft_strlen(char *str);
+char	*ft_joinofgnl(char *s1, char *s2);
+int		ft_hasnewline(char *str);
+char	*ft_majtmp(char *tmp);
+char	*ft_newline(char *tmp);
+
+#endif
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: user42 <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/11/24 11:29:25 by user42            #+#    #+#             */
+/*   Updated: 2020/11/24 16:13:05 by user42           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+
+size_t	ft_strlen(char *s)
+{
+	unsigned int i;
+
+	i = 0;
+	if (!s)
+		return (0);
+	while (s[i])
+		i++;
+	return (i);
+}
+
+char	*ft_joinofgnl(char *s1, char *s2)
+{
+	char			*res;
+	unsigned int	i;
+	unsigned int	j;
+	unsigned int	len;
+
+	if ((!s1 && !s2) || !(res = malloc(sizeof(*res) * \
+	(ft_strlen(s1) + ft_strlen(s2) + 1))))
+		return (NULL);
+	i = 0;
+	j = 0;
+	len = ft_strlen(s1);
+	while (i < len)
+	{
+		res[i] = s1[i];
+		i++;
+	}
+	len = ft_strlen(s2);
+	while (j < len)
+	{
+		res[i + j] = s2[j];
+		j++;
+	}
+	res[i + j] = '\0';
+	free(s1);
+	return (res);
+}
